f316969b90717ca07530e74de068b6053814e99f
Julius Olaifa <julius.olaifa@okstate.edu>
2022-02-10 16:12:39 -0600

    Initial Commit

---
+++ Ind02_Olaifa_Julius/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind02_Olaifa_Julius
+//
+//  Created by Julius Olaifa on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
